09e6c5ee83e32b49b6c0fce49a8e240eab9b6076
Emil Litwiniec <elitwiniec@apptension.com>
2025-02-25 20:01:38 +0100

    build: Final improvements

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
-console.warn("DEBUG[77]: next.config.ts:4: token=", token);
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
