c754c8e6297df3bcbf0a7e5c4f59f5dc5e699dfc
Emil Litwiniec <elitwiniec@apptension.com>
2025-02-25 19:31:41 +0100

    build: Populate secrets in pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+console.warn("DEBUG[77]: next.config.ts:4: token=", token);
 const nextConfig: NextConfig = {
   output: "export",
   images: {
